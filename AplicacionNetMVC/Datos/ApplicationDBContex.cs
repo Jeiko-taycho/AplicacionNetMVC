c23e619da9f6fca0d059325d30102e5c40e8b51a
jeikobbl <jeikobjga@gmail.com>
2022-08-30 09:42:50 -0600

    Add project files.

---
+++ AplicacionNetMVC/Datos/ApplicationDBContex.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AplicacionNetMVC.Datos
+{
+    public class ApplicationDBContex : DbContext
+    {
+        public ApplicationDBContex(DbContextOptions<ApplicationDBContex>)
+        {
+
+        }
+
+    }
+}
